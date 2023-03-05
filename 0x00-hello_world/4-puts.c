cc1862b4128018e22b0e306411b5e4d7bdc705ce
Adedayo <adedayoshittu97@gmail.com>
2023-03-05 08:01:23 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - "Programming is like building a multilingual puzzle, then new line."
- * return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
